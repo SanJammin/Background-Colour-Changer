e098fbb1de5c82fa77893b962ed2c919425cad18
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-04-11 19:23:56 +0100

    Set up colour changer

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const colours = ["#FFADAD", "#FFD6A5", "#FDFFB6", "#CAFFBF", "#A0C4FF", "#BDB2FF", "#FFC6FF", "#D0F4DE"];
+const colors = ["#FFADAD", "#FFD6A5", "#FDFFB6", "#CAFFBF", "#A0C4FF", "#BDB2FF", "#FFC6FF", "#D0F4DE"];
+const changeColorBtn = document.getElementById("change-color-btn");
+const currentColor = document.getElementById("current-color");
+
+changeColorBtn.addEventListener("click", () => {
+    let randomColor = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[randomColor];
+    currentColor.textContent = `Current Colour: ${colors[randomColor]}`;
+});
